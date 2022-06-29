c1a02139118bbb0e41bd19a36c4e218aa3cf65f3
Vishal Rathod <73741136+vishalok@users.noreply.github.com>
2022-06-29 22:46:40 +0530

    Add files via upload

---
+++ recursion.js
@@ -0,0 +1,12 @@
+function printTable (n,i) {
+    if(i>10){
+        return 0;
+    }else{
+        console.log(`${n} x ${i} = ${n * i }`);
+        i++;
+    }
+  
+    printTable(n,i);
+}
+
+printTable(5,1);
