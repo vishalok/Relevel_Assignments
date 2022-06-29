346b8029442b44c909aa4b260958c69dc5f81e08
Vishal Rathod <73741136+vishalok@users.noreply.github.com>
2022-06-29 22:42:18 +0530

    using single array

--- min-maxArray.js
+++ min-maxArray.js
@@ -3,6 +3,7 @@
 
 function numberArray (numArray){
 let minValue = numArray[0];
+let maxValue = numArray[0];
 
 for(let i=0;i<=numArray.length -1;i++){
     
@@ -10,28 +11,18 @@ for(let i=0;i<=numArray.length -1;i++){
     
     minValue = numArray[i];
     }
+    
+    if(numArray[i] > maxValue){
+    
+        maxValue = numArray[i];
+        }
+
 }
 
 console.log("minimum value = ",minValue);
+console.log("maximum number = ",maxValue);
 }
+
 
 numberArray([578,78,247,14,11]);
 
-/**************** */
-
-
-//let numArray1 = [234,12,344,4]; //defined array
-function maxArray (numArray1){
-let maxValue = numArray1[0];
-
-for(let i=0;i<=numArray1.length -1;i++){
-    
-   
-    if(numArray1[i] > maxValue){
-    
-    maxValue = numArray1[i];
-    }
-}
-console.log("maximum number = ",maxValue);
-}
-maxArray([378,47,52,41,4]);
