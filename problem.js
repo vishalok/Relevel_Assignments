4f27787e48d003a6ff780bb4e52dfb5d253de0b7
Vishal Rathod <73741136+vishalok@users.noreply.github.com>
2022-07-08 13:20:10 +0530

    multiply array elements by 5

---
+++ problem.js
@@ -0,0 +1,11 @@
+/**
+ * multiply each element in array  by 5
+ * input -> a= [1,3,5,7]
+ * output -> b = [5,15,25,35]
+ */
+
+let array = [1,3,5,7];
+
+for(let arr of array){
+    console.log(arr * 5);
+}
